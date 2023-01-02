a455af1b644aa9375f119e48527f9ed725cd9dbe
Ariel Gencianeo <gencianeo.arield@gmail.com>
2023-01-02 15:31:46 +0800

    today's update

---
+++ operators.py
@@ -0,0 +1,24 @@
+# variables
+a=2
+b=1
+
+# arithmetic operations
+#addition
+print(a+b)
+#subtraction
+print(a-b)
+#multiplication
+print(a*b)
+#division
+print(a/b)
+#modulos
+print(a%b)
+#exponentiation
+print(a**b)
+#floor divison
+print(a//b)
+print(a&b)
+print(a|b)
+print(a^b)
+print(a<<b)
+print(a>>b)
